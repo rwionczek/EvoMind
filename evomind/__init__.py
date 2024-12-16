a6464b0408a101ba70964f9ff25b24300e46540f
Radek <rwionczek@gmail.com>
2024-12-16 22:23:56 +0100

    Successful fast Acrobot learning in 10 episodes

--- evomind/__init__.py
+++ evomind/__init__.py
@@ -3,16 +3,19 @@ from collections import deque
 
 import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Policy(torch.nn.Module):
     def __init__(self, input_features, output_features):
         super().__init__()
-        self.first_layer = torch.nn.Linear(input_features, 100)
-        self.second_layer = torch.nn.Linear(100, output_features)
+        self.first_layer = torch.nn.Linear(input_features, 64)
+        self.second_layer = torch.nn.Linear(64, 64)
+        self.third_layer = torch.nn.Linear(64, output_features)
 
     def forward(self, input):
         x = torch.relu(self.first_layer(input))
-        x = self.second_layer(x)
+        x = torch.relu(self.second_layer(x))
+        x = self.third_layer(x)
         return x
 
 
@@ -24,57 +27,77 @@ class MemoryChunk:
         self.next_observation = next_observation
 
     def __repr__(self):
-        return "\n" + str(self.observation) + "; " + str(self.action) + "; " + str(self.reward)
+        return "\n" + str(self.observation) + "; " + str(self.action) + "; " + str(self.reward) + "; " + str(self.next_observation)
 
 class Agent:
     def __init__(self, observation_space, action_space):
         self.action_space = action_space
-        self.policy = Policy(observation_space, self.action_space)
+        self.policy_network = Policy(observation_space, self.action_space).to(device)
+        self.target_network = Policy(observation_space, self.action_space).to(device)
         self.previous_observation = None
         self.observation = None
         self.action = None
-        self.memory = deque(maxlen=100)
-        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=1e-3)
+        self.memory = deque(maxlen=10000)
+        self.optimizer = torch.optim.Adam(self.policy_network.parameters(), lr=1e-3)
+        self.exploration_probability = 1.0
 
-    def set_observation(self, observation):
-        if self.observation is not None:
-            self.previous_observation = self.observation
+        self.target_network.load_state_dict(self.policy_network.state_dict())
+
+    def set_observation(self, observation, reward):
+        if reward is not None:
+            self.memory.append(MemoryChunk(self.observation, self.action, reward, observation))
 
         self.observation = observation
 
     def choose_action(self):
-        if random.random() < 0.05:
+        if random.random() <= self.exploration_probability:
             self.action = random.choice(range(self.action_space))
         else:
-            q_values = self.policy(torch.tensor(self.observation, dtype=torch.float32))
-            self.action = q_values.argmax().item()
+            with torch.no_grad():
+                q_values = self.policy_network(torch.tensor(self.observation, dtype=torch.float32, device=device))
+                self.action = q_values.argmax().item()
 
         return self.action
 
-    def set_reward(self, reward):
-        self.memory.append(MemoryChunk(self.observation, self.action, reward, self.previous_observation))
-
-    def learn(self):
-        batch_size = 100
+    def learn(self, print_loss=False):
+        batch_size = 64
 
         if len(self.memory) < batch_size:
             return
 
         batch = random.sample(self.memory, batch_size)
 
-        observations = torch.tensor([m.observation for m in batch], dtype=torch.float32)
-        actions = torch.tensor([m.action for m in batch], dtype=torch.int64)
-        rewards = torch.tensor([m.reward for m in batch], dtype=torch.float32)
-        next_observations = torch.tensor([m.next_observation for m in batch], dtype=torch.float32)
+        observations = torch.tensor([m.observation for m in batch], dtype=torch.float32, device=device)
+        actions = torch.tensor([m.action for m in batch], dtype=torch.int64, device=device)
+        rewards = torch.tensor([m.reward for m in batch], dtype=torch.float32, device=device)
+        next_observations = torch.tensor([m.next_observation for m in batch], dtype=torch.float32, device=device)
 
-        q_values = self.policy(observations).gather(1, actions.unsqueeze(1)).squeeze()
+        q_values = self.policy_network(observations).gather(1, actions.unsqueeze(1)).squeeze()
 
         with torch.no_grad():
-            next_q_values = self.policy(next_observations).max(1)[0]
+            next_q_values = self.target_network(next_observations).max(1)[0]
             target_q_values = rewards + 0.99 * next_q_values
 
-        self.optimizer.zero_grad()
         loss = torch.nn.functional.mse_loss(q_values, target_q_values)
+        self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+
+        if print_loss:
+            print(f'Loss: {loss.item()}')
+
+        self._soft_update_target_network()
+
+    def set_exploration_probability(self, probability):
+        self.exploration_probability = probability
+
+    def _soft_update_target_network(self):
+        TAU = 0.05
+        target_network_state_dict = self.target_network.state_dict()
+        policy_network_state_dict = self.policy_network.state_dict()
+
+        for key in policy_network_state_dict:
+            target_network_state_dict[key] = policy_network_state_dict[key] * TAU + target_network_state_dict[key]  * (1 - TAU)
+
+        self.target_network.load_state_dict(target_network_state_dict)
 
--- main.py
+++ main.py
@@ -1,24 +1,56 @@
 import gymnasium
+import matplotlib.pyplot as plt
 
 from evomind import Agent
 
-environment = gymnasium.make('Acrobot-v1', max_episode_steps=100000, render_mode='human')
+environment = gymnasium.make('Acrobot-v1', render_mode='human')
 agent = Agent(environment.observation_space.shape[0], environment.action_space.n)
 
+total_rewards = []
+for episode in range(10):
+    observation, info = environment.reset()
+    agent.set_observation(observation, None)
+
+    reward = None
+    total_reward = 0
+    while True:
+        observation, reward, terminated, truncated, info = environment.step(agent.choose_action())
+        agent.set_observation(observation, reward)
+        agent.learn(print_loss=False)
+
+        total_reward += reward
+
+        if terminated:
+            break
+
+        if truncated:
+            break
+
+    total_rewards.append(total_reward)
+
+    agent.set_exploration_probability(agent.exploration_probability * 0.7)
+
+    print(f'Exploration probability: {agent.exploration_probability}')
+    print(f'Episode {episode}')
+
+environment.close()
+
+environment = gymnasium.make('Acrobot-v1', render_mode='human')
 observation, info = environment.reset()
 
-step_count = 0
+agent.set_exploration_probability(0.0)
+
+reward = None
 while True:
-    agent.set_observation(observation)
+    agent.set_observation(observation, reward)
     observation, reward, terminated, truncated, info = environment.step(agent.choose_action())
-    agent.set_reward(reward)
-
-    if step_count % 100 == 0:
-        agent.learn()
-
-    step_count += 1
 
     if terminated or truncated:
         break
-
 environment.close()
+
+plt.plot(total_rewards)
+plt.title('Total Rewards Over Episodes')
+plt.xlabel('Episode')
+plt.ylabel('Total Reward')
+plt.show()
