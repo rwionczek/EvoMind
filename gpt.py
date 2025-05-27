d6505cdb912eb485fe10c730fef138e79f0d43b0
Radek <rwionczek@gmail.com>
2025-05-27 20:24:01 +0200

    Implement transformer model into environment

---
+++ gpt.py
@@ -0,0 +1,202 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+block_size = 100
+batch_size = 16
+max_iters = 100
+learning_rate = 3e-4
+eval_iters = 100
+n_embd = 384
+n_head = 8
+n_layer = 8
+dropout = 0.2
+
+
+# chars = ""
+# with open('wizard_of_oz.txt', 'r', encoding='utf-8') as f:
+#     text = f.read()
+#     chars = sorted(list(set(text)))
+#
+# vocab_size = len(chars)
+#
+# string_to_int = {ch: i for i, ch in enumerate(chars)}
+# int_to_string = {i: ch for i, ch in enumerate(chars)}
+# encode = lambda s: [string_to_int[c] for c in s]
+# decode = lambda l: ''.join([int_to_string[i] for i in l])
+#
+# data = torch.tensor(encode(text), dtype=torch.long)
+#
+# n = int(0.8 * len(data))
+# train_data = data[:n]
+# val_data = data[n:]
+
+
+# def get_batch(split):
+#     data = train_data if split == 'train' else val_data
+#     ix = torch.randint(len(data) - block_size, (batch_size,))
+#     x = torch.stack([data[i:i + block_size] for i in ix])
+#     y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
+#     x, y = x.to(device), y.to(device)
+#     return x, y
+
+
+# @torch.no_grad()
+# def estimate_loss():
+#     out = {}
+#     model.eval()
+#     for split in ['train', 'val']:
+#         losses = torch.zeros(eval_iters)
+#         for k in range(eval_iters):
+#             X, Y = get_batch(split)
+#             logits, loss = model(X, Y)
+#             losses[k] = loss.item()
+#         out[split] = losses.mean()
+#     model.train()
+#     return out
+
+
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+
+        wei = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class GPTModel(nn.Module):
+    def __init__(self, memory_sequence_chunk_size):
+        super().__init__()
+        self.input_layer = nn.Linear(memory_sequence_chunk_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.lm_head = nn.Linear(n_embd, memory_sequence_chunk_size)
+
+    def __init_weights(self, module):
+        if (isinstance(module, nn.Linear)):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, memory_sequence, targets=None):
+        B, T, C = memory_sequence.shape
+
+        input = self.input_layer(memory_sequence)
+
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+        x = input + pos_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        memory_sequence_chunk = self.lm_head(x)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = memory_sequence_chunk.shape
+            memory_sequence_chunk = memory_sequence_chunk.view(B * T, C)
+            targets = targets.view(B * T, C)
+            loss = F.mse_loss(memory_sequence_chunk, targets)
+
+        return memory_sequence_chunk, loss
+
+    def generate(self, index, max_new_tokens):
+        for _ in range(max_new_tokens):
+            index_cond = index[:, -block_size:]
+            logits, loss = self.forward(index_cond)
+            logits = logits[:, -1, :]
+            props = F.softmax(logits, dim=1)
+            index_next = torch.multinomial(props, num_samples=1)
+            index = torch.cat((index, index_next), dim=1)
+        return index
+
+# model = GPTModel(vocab_size)
+# m = model.to(device)
+#
+# context = torch.zeros((1, 1), dtype=torch.long, device=device)
+# generated_chars = decode(m.generate(context, max_new_tokens=500)[0].tolist())
+# print(generated_chars)
+#
+# optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+#
+# for iter in range(max_iters):
+#     if iter % eval_iters == 0:
+#         losses = estimate_loss()
+#         print(f"step: {iter}, train loss: {losses['train']:.4f}, val loss: {losses['val']:.4f}")
+#
+#     xb, yb = get_batch('train')
+#
+#     logits, loss = model.forward(xb, yb)
+#     optimizer.zero_grad(set_to_none=True)
+#     loss.backward()
+#     optimizer.step()
+#
+# print(loss.item())
+#
+# context = torch.zeros((1, 1), dtype=torch.long, device=device)
+# generated_chars = decode(m.generate(context, max_new_tokens=500)[0].tolist())
+# print(generated_chars)
--- main.py
+++ main.py
@@ -1,52 +1,96 @@
-from collections import deque
-
 import gymnasium
 import numpy as np
 import torch
 
-from memory import Memory, MemorySequence, MemorySequenceChunk
+from gpt import GPTModel
 
-environment = gymnasium.make('CartPole-v1')
+memory_capacity = 128
+memory_sequence_length = 100
+
+environment = gymnasium.make('CartPole-v1', render_mode='human')
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class TransformerActionPredictor(torch.nn.Module):
-    def __init__(self, input_dim, n_actions, d_model=64, nhead=8, num_layers=3):
-        super().__init__()
-        self.embedding = torch.nn.Linear(input_dim, d_model)
-        encoder_layer = torch.nn.TransformerEncoderLayer(d_model, nhead)
-        self.transformer = torch.nn.TransformerEncoder(encoder_layer, num_layers)
-        self.decoder = torch.nn.Linear(d_model, n_actions)
+memory_sequence_chunk_length = environment.observation_space.shape[0] + environment.action_space.n + 1
+memory = torch.zeros(memory_capacity, memory_sequence_length, memory_sequence_chunk_length).to(device)
+memory_values = torch.zeros(memory_capacity, 1).to(device)
+memory_sequence = torch.zeros(memory_sequence_length, memory_sequence_chunk_length).to(device)
 
-    def forward(self, src):
-        embedded = self.embedding(src)
-        transformer_out = self.transformer(embedded)
-        output = self.decoder(transformer_out[-1])
-        return output
+model = GPTModel(memory_sequence_chunk_length).to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+
+learn_memory_sequence_length = 8
+learn_batch_size = 16
 
 
-model = TransformerActionPredictor(input_dim=environment.observation_space.shape[0],
-                                   n_actions=environment.action_space.n).to(device)
-memory = Memory(10)
+def get_memory_batch():
+    im = torch.randint(memory_capacity, (learn_batch_size,))
+    ix = torch.randint(memory_sequence_length - learn_memory_sequence_length, (learn_batch_size,))
+    x = torch.stack([memory[im[i], ix[i]:ix[i] + learn_memory_sequence_length] for i in range(learn_batch_size)])
+    y = torch.stack(
+        [memory[im[i], ix[i] + 1:ix[i] + learn_memory_sequence_length + 1] for i in range(learn_batch_size)])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+eval_iters = 10
+
+
+@torch.no_grad()
+def estimate_loss():
+    model.eval()
+    losses = torch.zeros(eval_iters)
+    for k in range(eval_iters):
+        X, Y = get_memory_batch()
+        logits, loss = model(X, Y)
+        losses[k] = loss.item()
+    out = losses.mean()
+    model.train()
+    return out
+
 
 total_rewards = []
-for episode in range(20):
+for episode in range(1000):
     observation, info = environment.reset()
 
-    reward = None
+    # reward = None
     total_reward = 0
-    memory_sequence = MemorySequence()
+
     while True:
-        action = environment.action_space.sample()
+
+        with torch.no_grad():
+            prediction, _ = model(memory_sequence.unsqueeze(0))
+            action_probabilities = torch.softmax(prediction[:, -1, environment.observation_space.shape[0]:
+                                                                   environment.observation_space.shape[
+                                                                       0] + environment.action_space.n], dim=-1)
+            action = action_probabilities.multinomial(num_samples=1).item()
         previous_observation = observation
         observation, reward, terminated, truncated, info = environment.step(
             action
         )
 
         if terminated:
-            reward = -100
+            reward = -10
 
-        memory_sequence.add_chunk(MemorySequenceChunk(previous_observation, action, reward))
+        action_tensor = torch.zeros(environment.action_space.n, device=device)
+        action_tensor[action] = 1.0
+
+        new_sequence = torch.cat([
+            torch.tensor(observation, dtype=torch.float32, device=device),
+            action_tensor,
+            torch.tensor([reward], dtype=torch.float32, device=device)
+        ])
+
+        memory_sequence = torch.roll(memory_sequence, -1, dims=0)
+        memory_sequence[-1] = new_sequence
+
+        memory_sequence_value = memory_sequence[:, -1].sum()
+
+        lower_value_indices = torch.where(memory_values <= memory_sequence_value)[0]
+        if len(lower_value_indices) > 0:
+            random_index = lower_value_indices[torch.randint(0, len(lower_value_indices), (1,))]
+            memory[random_index] = memory_sequence
+            memory_values[random_index] = memory_sequence_value
 
         total_reward += reward
 
@@ -56,10 +100,26 @@ for episode in range(20):
         if truncated:
             break
 
-    memory.add(memory_sequence)
+    memory_rewards = memory[:, :, -1]
+    memory_value = memory_rewards.sum()
+    print('Memory value: %.1f' % memory_value.item())
+
+    if (episode + 1) % 10 == 0:
+        for iter in range(300):
+            if iter % eval_iters == 0:
+                losses = estimate_loss()
+                print(f"step: {iter}, loss: {losses:.4f}")
+
+            xb, yb = get_memory_batch()
+
+            logits, loss = model.forward(xb, yb)
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
 
     total_rewards.append(total_reward)
 
-    print('Episode ', episode, '; Total reward %.1f' % total_reward, '; 100 game average %.1f' % np.mean(total_rewards[-100:]))
+    print('Episode ', episode, '; Total reward %.1f' % total_reward,
+          '; 100 game average %.1f' % np.mean(total_rewards[-100:]))
 
 environment.close()
